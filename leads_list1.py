b120b1a82296fe4a3c227f448592a0d3455cb951
0xSector <107321063+0xSector@users.noreply.github.com>
2023-06-07 22:26:50 -0500

    Update leads_list1.py

--- leads_list1.py
+++ leads_list1.py
@@ -1,84 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-
-# def generate_table():
-#     data = {
-#         'Wallet/User': ['0x29ab82Ec552573b1B7d4933B2AaA3C568be9C6D1', '0xBE7CE5358977B00868C52045fb6c2790514efE68', '0xc9a648CF30D16079e48404F6Fd1Fd7Be83650C4d', '0xB5A16b3962BAcEB2CB292047333b58AaD1E060EE'],
-#         'Wallet Value (USD)': [1000, 500, 2000, 1500]*5,
-#         'Gambling User Y/N': ['N', 'Y', 'N', 'Y']*5,
-#         'Deposit Value': [500, 1000, 1500, 200]*5,
-#         'TLV': [1500, 1500, 3500, 1700]*5,
-#         'Last Transaction Date': ['2022-05-01', '2023-03-15', '2023-04-20', '2023-05-25']*5,
-#         'Email': ['John33@gmail.com', 'Allenkjb@gmail.com', 'Opal@gmail.com', 'CryptoJacob@hotmail.com']*5,
-#         'Twitter': ['@John33', '@Allenkjb', '@Opal', '@CryptoJacob']*5,
-#         'Telegram': ['@John33', '@Allenkjb', '@Opal', '@CryptoJacob']*5,
-#         'Discord': ['John33#1234', 'Allenkjb#1234', 'Opal#1234', 'CryptoJacob#1234']*5,
-#         'linkedin': ['John33', 'Allenkjb', 'Opal', 'CryptoJacob']*5,
-#     }
-
-#     df = pd.DataFrame(data)
-#     return df
-
-# def main():
-#     st.title('Leads List - Gambling Users')
-#     df = generate_table()
-#     st.dataframe(df)
-
-# if __name__ == '__main__':
-#     main()
-
-
-
-
-# import streamlit as st
-# import pandas as pd
-
-
-# # Define the tables
-# tables = {
-
-      
-#     'Gambling Leads': {
-#         'Wallet/User': ['0x29ab82Ec552573b1B7d4933B2AaA3C568be9C6D1', '0xBE7CE5358977B00868C52045fb6c2790514efE68', '0xc9a648CF30D16079e48404F6Fd1Fd7Be83650C4d', '0xB5A16b3962BAcEB2CB292047333b58AaD1E060EE']*5,
-#         'Wallet Value (USD)': [1000, 500, 2000, 1500]*5,
-#         'Gambling User Y/N': ['N', 'Y', 'N', 'Y']*5,
-#         'Deposit Value': [500, 1000, 1500, 200]*5,
-#         'TLV': [15000, 1500, 350000, 17000]*5,
-#         'Last Transaction Date': ['2022-05-01', '2023-03-15', '2023-04-20', '2023-05-25']*5,
-#         'Email': ['John33@gmail.com', 'Allenkjb@gmail.com', 'Opal@gmail.com', 'CryptoJacob@hotmail.com']*5,
-#         'Twitter': ['@John33', '@Allenkjb', '@Opal', '@CryptoJacob']*5,
-#         'Telegram': ['@John33', '@Allenkjb', '@Opal', '@CryptoJacob']*5,
-#         'Discord': ['John33#1234', 'Allenkjb#1234', 'Opal#1234', 'CryptoJacob#1234']*5,
-#         'linkedin': ['John33', 'Allenkjb', 'Opal', 'CryptoJacob']*5,
-#     },
-#     'Liquidity Provider Leads': {
-#         'Wallet/User': ['0x29ab82Ec552573b1B7d4933B2AaA3C568be9C6D1', '0xBE7CE5358977B00868C52045fb6c2790514efE68', '0xc9a648CF30D16079e48404F6Fd1Fd7Be83650C4d', '0xB5A16b3962BAcEB2CB292047333b58AaD1E060EE']*5,
-#         'Wallet Value (USD)': [1000, 500, 2000, 1500]*5,
-#         'Liquidity Provider Y/N': ['N', 'Y', 'N', 'Y']*5,
-#         'Deposit Value': [500, 1000, 1500, 200]*5,
-#         'TLV': [1500, 1500, 3500, 1700]*5,
-#         'Last Transaction Date': ['2022-05-01', '2023-03-15', '2023-04-20', '2023-05-25']*5,
-#         'Email': ['John33@gmail.com', 'Allenkjb@gmail.com', 'Opal@gmail.com', 'CryptoJacob@hotmail.com']*5,
-#         'Twitter': ['@John33', '@Allenkjb', '@Opal', '@CryptoJacob']*5,
-#         'Telegram': ['@John33', '@Allenkjb', '@Opal', '@CryptoJacob']*5,
-#         'Discord': ['John33#1234', 'Allenkjb#1234', 'Opal#1234', 'CryptoJacob#1234']*5,
-#         'linkedin': ['John33', 'Allenkjb', 'Opal', 'CryptoJacob']*5,
-#     }
-# }
-
-# def main():
-#     st.title('Wallet Information')
-
-#     # Create a dropdown selector in the sidebar
-#     table_selection = st.sidebar.selectbox('Select Table', list(tables.keys()))
-
-#     # Display the selected table in wide mode
-#     df = pd.DataFrame(tables[table_selection])
-#     st.dataframe(df, width=800)
-
-# if __name__ == '__main__':
-#     main()
-
 
 import streamlit as st
 import pandas as pd
@@ -86,7 +5,7 @@ import random
 
 # Define the tables
 tables = {
-  'GLP + Binance Users': {
+  'GLP Holders + Binance Example Leads': {
         'Wallet Address': ['0x016dbc709b8c1667d7205e2c4129167d660dc010', '0x26013b787aac632a92053f669e2de85103ad2536', '0xbc17b5a63fa8fdf28220546bc24b0beb10e2c80f', '0x92f3919d142000396205c613ecd2e428d91cf9220', '0x3615e04d0f21e2c7d2051e561ce8d4a5d0594ee7', '0x2465bd53a0e4f726d289bf059e07979715c44dc0b', '0xde58c7b2335c895c27471a6f237c78b066924370', '0xdc23d1367d84aad239913b8c8579c29e3707e309', '0x7706abe0d94e88760375dc3d0e997d5680324e38', '0xf9dbd46ec67dad89094fe788c29147e00fc25fe7', '0xa0553e045fda77d890741ffd5b58ae7cefdab379', '0xa0553e045fda77890e741ffd5b58ae7cefdab380', '0x016dbc709b8c1667d7205e2c4129167d660dc010', '0x26013b787aac632a92053f669e2de85103ad2536', '0x06a30395353d7d3742e49bf69fd25fdf69a131c8', '0xbc17b5a63fa8fdf28220546bc24b0beb10e2c80f', '0x3615e04d0f21e2c7d2051e561ce8d4a5d0594ee7', '0xbc17b5a63fa8fdf28220546bc24b0beb10e2c80f', '0x016dbc709b8c1667d7205e2c4129167d660dc010', '0xdc23d1367d84aad239913b8c8579c29e3707e309'],
         'GLP Held (USD Value)': ['$5,452.93', '$6,537.06', '$11,117.74', '$11,237.44', '$16,276.17', '$24,459.51', '$3,943.03', '$3,887.21', '$3,509.44', '$945,271.05', '$4,154.09', '$2,986.18', '$2,919.87', '$2,712.84', '$4,434.13', '$4,687.85', '$5,964.27', '$40,734.84', '$16,726.15', '$9,923.58'],
         'Wallet (USD Value)': ['$81,793.95', '$98,055.90', '$166,766.10', '$168,561.56', '$244,142.55', '$366,892.65', '$59,145.45', '$58,308.15', '$52,641.60', '$14,179,065.75', '$62,311.35', '$44,792.70', '$43,798.05', '$40,692.60', '$66,511.89', '$70,317.75', '$89,464.05', '$611,022.60', '$250,892.25', '$148,853.70'],
@@ -168,7 +87,7 @@ tables = {
 def main():
     # Set page configuration
     st.set_page_config(
-        page_title='Leads Lists by Sector',
+        page_title='Arrel Example Leads',
         page_icon='💰',
         layout='wide',
         initial_sidebar_state='expanded'
