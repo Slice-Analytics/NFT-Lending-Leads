87509f4e5b3f0d16c14e4155ce78db21d47b5be0
0xSector <107321063+0xSector@users.noreply.github.com>
2023-06-19 09:27:42 -0500

    Update leads_list1.py

--- leads_list1.py
+++ leads_list1.py
@@ -158,7 +158,7 @@ def main():
 
 
     cols_titles = ['Total Wallets Tracked', 'Leads Available', 'Fees Generated', 'Trades Completed']
-    cols_data = [Metrics.get(table_selection,{}).get('Total Wallets','NA'), Metrics.get(table_selection,{}).get('Life Time Value','NA'), Metrics.get(table_selection,{}).get('Fees Generated','NA'), Metrics.get(table_selection,{}).get('Trades Completed','NA')]
+    cols_data = [Metrics.get(table_selection,{}).get('Total Wallets Tracked','NA'), Metrics.get(table_selection,{}).get('Total Wallets Tracked','NA'), Metrics.get(table_selection,{}).get('Fees Generated','NA'), Metrics.get(table_selection,{}).get('Trades Completed','NA')]
     cols = st.columns(len(cols_titles))
 
     for i in range(len(cols_titles)):
